24a521b8ea6ff6dcb591300d7d33b840304d4c89
adamfisk <a@littleshoot.org>
2010-12-19 22:35:58 +0000

    Formatting and answer check
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/p2p@7673 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/main/java/org/lastbamboo/common/p2p/DefaultTcpUdpSocket.java
+++ src/main/java/org/lastbamboo/common/p2p/DefaultTcpUdpSocket.java
@@ -21,8 +21,7 @@ import org.slf4j.LoggerFactory;
  * reliable UDP connection, depending on which successfully connects first.
  */
 public class DefaultTcpUdpSocket implements TcpUdpSocket, 
-    OfferAnswerTransactionListener, OfferAnswerListener
-    {
+    OfferAnswerTransactionListener, OfferAnswerListener {
 
     private final Logger m_log = LoggerFactory.getLogger(getClass());
     private final long m_startTime = System.currentTimeMillis();
@@ -57,27 +56,22 @@ public class DefaultTcpUdpSocket implements TcpUdpSocket,
      * @throws IOException If there's an error connecting.
      */
     public DefaultTcpUdpSocket(final Offerer offerer,
-        final OfferAnswerFactory offerAnswerFactory,
-        final int relayWaitTime) throws IOException
-        {
+        final OfferAnswerFactory offerAnswerFactory, final int relayWaitTime)
+        throws IOException {
         this.m_offerer = offerer;
         this.m_relayWaitTime = relayWaitTime;
-        try
-            {
+        try {
             this.m_offerAnswer = offerAnswerFactory.createOfferer(this);
-            }
-        catch (final OfferAnswerConnectException e)
-            {
+        } catch (final OfferAnswerConnectException e) {
             throw new IOExceptionWithCause("Could not create offerer", e);
-            }
         }
-    
-    public Socket newSocket(final URI uri) throws IOException
-        {
+    }
+
+    public Socket newSocket(final URI uri) throws IOException {
         final byte[] offer = this.m_offerAnswer.generateOffer();
         this.m_offerer.offer(uri, offer, this);
         return waitForSocket(uri);
-        }
+    }
 
     /**
      * Waits for the successful creation of a socket or a socket creation
@@ -87,181 +81,149 @@ public class DefaultTcpUdpSocket implements TcpUdpSocket,
      * @return The new socket.
      * @throws IOException If there's any problem creating the socket.
      */
-    private Socket waitForSocket(final URI sipUri) throws IOException
-        {
-        synchronized (this.m_answerLock)
-            {
-            if (!this.m_gotAnswer)
-                {
-                try 
-                    {
+    private Socket waitForSocket(final URI sipUri) throws IOException {
+        synchronized (this.m_answerLock) {
+            if (!this.m_gotAnswer) {
+                try {
                     this.m_answerLock.wait(20 * 1000);
-                    } 
-                catch (final InterruptedException e) 
-                    {
+                } catch (final InterruptedException e) {
                     m_log.error("Interrupted?", e);
-                    }
                 }
             }
-    
+        }
+        
+        if (!this.m_gotAnswer) {
+            m_log.info("Did not get an answer from: "+sipUri);
+            throw new IOException("Did not get an answer from: "+sipUri);
+        }
+
         m_log.info("Got answer...");
-        synchronized (this.m_socketLock)
-            {
-            
+        synchronized (this.m_socketLock) {
+
             // We use this flag in case we're notified of the socket before
-            // we start waiting. We'd wait forever in that case without this 
+            // we start waiting. We'd wait forever in that case without this
             // check.
-            if (!m_finishedWaitingForSocket)
-                {
+            if (!m_finishedWaitingForSocket) {
                 m_log.trace("Waiting for socket...");
-                try
-                    {
+                try {
                     // We add one to make sure we don't sleep forever.
                     m_socketLock.wait((this.m_relayWaitTime * 1000) + 1);
-                    }
-                catch (final InterruptedException e)
-                    {
+                } catch (final InterruptedException e) {
                     // Should never happen -- we don't use interrupts here.
                     m_log.error("Unexpectedly interrupted", e);
-                    }
                 }
+            }
 
-            if (this.m_socket == null)
-                {
+            if (this.m_socket == null) {
                 // If the socket is still null, we could not create a direct
                 // connection. Instead we'll have to relay the data.
                 m_log.info("Could not create direct connection - using relay!");
                 this.m_offerAnswer.useRelay();
                 m_log.trace("Waiting for socket...");
-                // We sometimes have to wait for awhile for resolution, 
+                // We sometimes have to wait for awhile for resolution,
                 // especially if we're accessing a file from around the world!!
-                try
-                    {
+                try {
                     m_socketLock.wait(35 * 1000);
-                    }
-                catch (final InterruptedException e)
-                    {
+                } catch (final InterruptedException e) {
                     // Should never happen -- we don't use interrupts here.
                     m_log.error("Unexpectedly interrupted", e);
-                    }
                 }
-            
-            // If the socket is still null, that means even the relay failed 
-            // for some reason. This should never happen, but it's of course 
+            }
+
+            // If the socket is still null, that means even the relay failed
+            // for some reason. This should never happen, but it's of course
             // possible.
-            if (this.m_socket == null)
-                {
+            if (this.m_socket == null) {
                 m_log.warn("Socket is null...");
-                
+
                 // This notifies IceAgentImpl that it should close all its
                 // candidates.
                 this.m_offerAnswer.close();
-                throw new IOException("Could not connect to remote host: "+
-                    sipUri);
-                }
-            else
-                {
+                throw new IOException("Could not connect to remote host: "
+                        + sipUri);
+            } else {
                 m_log.trace("Returning socket!!");
                 return this.m_socket;
-                }
             }
         }
+    }
     
     /**
      * Simply notifies the socket lock that it should stop waiting.  This
      * will happen both when we've successfully created a socket and when
      * there's been an error creating the socket.
      */
-    private void notifySocketLock()
-        {
-        synchronized(this.m_socketLock)
-            {
+    private void notifySocketLock() {
+        synchronized (this.m_socketLock) {
             m_finishedWaitingForSocket = true;
             this.m_socketLock.notify();
-            }
         }
+    }
 
-    public void onTransactionSucceeded(final OfferAnswerMessage response) 
-        {
+    public void onTransactionSucceeded(final OfferAnswerMessage response) {
         m_log.trace("Received INVITE OK");
 
-        m_log.debug("Successful transaction after {} milliseconds...", 
-            getElapsedTime());
-        
+        m_log.debug("Successful transaction after {} milliseconds...",
+                getElapsedTime());
+
         // Determine the ICE candidates for socket creation from the
         // response body.
         final ByteBuffer answer = response.getBody();
-        
+
         m_gotAnswer = true;
-        
-        synchronized (this.m_answerLock) 
-            {
+
+        synchronized (this.m_answerLock) {
             this.m_answerLock.notifyAll();
-            }
-        
-        // This is responsible for notifying listeners on errors.
-        this.m_offerAnswer.processAnswer(answer);
         }
 
-    public void onTransactionFailed(final OfferAnswerMessage response)
-        {
-        m_log.warn("Failed transaction after " + getElapsedTime() +
-            " milliseconds...");
+        // This is responsible for notifying listeners on errors.
+        this.m_offerAnswer.processAnswer(answer);
+    }
+
+    public void onTransactionFailed(final OfferAnswerMessage response) {
+        m_log.warn("Failed transaction after " + getElapsedTime()
+                + " milliseconds...");
 
         // We know the status of the remote host, so make sure the socket
         // fails as quickly as possible.
         notifySocketLock();
         this.m_offerAnswer.close();
-        }
+    }
 
-    public void onTcpSocket(final Socket sock)
-        {
-        if (processedSocket(sock)) 
-            {
+    public void onTcpSocket(final Socket sock) {
+        if (processedSocket(sock)) {
             this.m_offerAnswer.closeUdp();
-            }
-        else 
-            {
+        } else {
             this.m_offerAnswer.closeTcp();
-            }
         }
-    
-    public void onUdpSocket(final Socket sock)
-        {
-        if (processedSocket(sock)) 
-            {
-            this.m_offerAnswer.closeTcp();
-            }
-        else 
-            {
-            this.m_offerAnswer.closeUdp();
-            }
-        }
+    }
 
-    private boolean processedSocket(final Socket sock)
-        {
-        if (m_socket != null)
-            {
+    public void onUdpSocket(final Socket sock) {
+        if (processedSocket(sock)) {
+            this.m_offerAnswer.closeTcp();
+        } else {
+            this.m_offerAnswer.closeUdp();
+        }
+    }
+
+    private boolean processedSocket(final Socket sock) {
+        if (m_socket != null) {
             m_log.info("Ignoring socket");
-            try
-                {
+            try {
                 sock.close();
-                }
-            catch (final IOException e)
-                {
-                }
-            return false;
+            } catch (final IOException e) {
             }
+            return false;
+        }
         m_socket = sock;
         notifySocketLock();
         return true;
-        }
+    }
 
-    public void onOfferAnswerFailed(final OfferAnswer offerAnswer)
-        {
+    public void onOfferAnswerFailed(final OfferAnswer offerAnswer) {
         notifySocketLock();
         this.m_offerAnswer.close();
-        }
+    }
     
     /**
      * Returns the elapsed time from the start time.  This method assumes that
@@ -269,11 +231,10 @@ public class DefaultTcpUdpSocket implements TcpUdpSocket,
      * 
      * @return The elapsed time from the start time.
      */
-    private long getElapsedTime()
-        {
+    private long getElapsedTime() {
         final long now = System.currentTimeMillis();
         final long elapsedTime = now - this.m_startTime;
-        
+
         return elapsedTime;
-        }
     }
+}
